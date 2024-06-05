18f3248ecd5785df911780cc2c9f7f344e266276
derThomasprivat <derThomasprivat@users.noreply.github.com>
2024-06-05 06:16:53 +0200

    Update dtBash.sh

--- powerline-go/dtBash.sh
+++ powerline-go/dtBash.sh
@@ -1,4 +1,4 @@
 mkdir ~/bin
 PATH=$PATH:~/bin
 wget -O ~/bin/powerline-go https://github.com/derThomasprivat/dtbash/raw/main/powerline-go/powerline-go-linux-amd64
-wget -O ~/.bashrc_ powerline-go https://github.com/derThomasprivat/dtbash/raw/main/powerline-go/.bashrc_ && source ~/.bashrc_
+wget -O ~/.bashrc_ https://github.com/derThomasprivat/dtbash/raw/main/powerline-go/.bashrc_ && source ~/.bashrc_
